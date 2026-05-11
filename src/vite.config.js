912f0fdc161cc441111d112e0507645ddeb9adc0
JIMMY GARCIA VALLEJOS <garushia29@gmail.com>
2026-05-11 01:05:38 -0400

    preparado de entorno de desarrollo con docker

---
+++ src/vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            host: '127.0.0.1',
+            protocol: 'ws',
+        },
+    },
+
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+});
+
